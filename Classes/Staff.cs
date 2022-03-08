7618a01756d8d12592d82bb39254672d7019f81a
FoxKing15 <emmie.bourassin@icloud.com>
2022-03-08 09:53:24 +0100

    update

--- Classes/Staff.cs
+++ Classes/Staff.cs
@@ -8,5 +8,10 @@ namespace EquiLog
 {
     internal class Staff
     {
+        public string LastName { get; }
+        public string FirstName { get; }
+        public string Job { get; set; }
+        public int Hours { get; set; }
+        public int Holidays { get; set; }
     }
 }
