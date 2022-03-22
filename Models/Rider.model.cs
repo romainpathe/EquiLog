fd1d63126948aea59b44a3b69ebffb00ac8ebd91
Romain Pathé <no_reply@romainpathe.com>
2022-03-22 11:13:24 +0100

    xx

--- Models/Rider.model.cs
+++ Models/Rider.model.cs
@@ -8,6 +8,18 @@
         public string Email { get; set; }
         public string Phone { get; set; }
         public int Course { get; set; }
-        public int Level { get; set; }
+        public string Level { get; set; }
+
+        public Rider_model(int id, string firstname, string lastname,string email,string phone, int course, string level)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+            Phone = phone;
+            Course = course;
+            Level = level;
+        }       
+        
     }
 }
--- Pages/Rider/Add.xaml.cs
+++ Pages/Rider/Add.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using EquiLog.Controllers;
 
 namespace EquiLog.Pages.Rider
 {
@@ -7,6 +9,48 @@ namespace EquiLog.Pages.Rider
         public Add()
         {
             InitializeComponent();
+        }
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (firstname.Text == "")
+            {
+                MessageBox.Show("Le prénom ne peux pas être null");
+                return;
+            }
+            if (lastname.Text == "")
+            {
+                MessageBox.Show("Le nom ne peux pas être null");
+                return;
+            }
+            if (level.Text == "")
+            {
+                MessageBox.Show("Le level ne peux pas être null");
+                return;
+            }
+            if (email.Text == "")
+            {
+                MessageBox.Show("L'email ne peux pas être null");
+                return;
+            }
+            if (phone.Text == "")
+            {
+                MessageBox.Show("Le téléphone ne peux pas être null");
+                return;
+            }
+            if (course.Text == "")
+            {
+                MessageBox.Show("Le nombre d'heure de cours ne peux pas être null");
+                return;
+            }
+            var x = int.TryParse(course.Text, out var intCourse);
+            if (!x)
+            {
+                MessageBox.Show("Les heures de cours doit être un nombres");
+                return;
+            }
+
+            Rider_controller.AddRider(firstname.Text, lastname.Text, email.Text,phone.Text,intCourse,level.Text);
+            Window.GetWindow(this)?.Close();
         }
     }
 }
