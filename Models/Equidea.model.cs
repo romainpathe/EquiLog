3dfb146e5fa61e679770613deddc436082fc645f
FoxKing15 <emmie.bourassin@icloud.com>
2022-03-28 15:06:13 +0100

    re update equidae

--- Models/Equidea.model.cs
+++ Models/Equidea.model.cs
@@ -6,8 +6,6 @@
         public string Gender { get; set; }  
         public int Age { get; set; }    
         public string Color { get; set; }   
-        public string Type { get; set; }    
-        public bool IsSick { get; set; }
         public int Hours { get; set; }
     }
 }
