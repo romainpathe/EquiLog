adae9220e02a11691a0a186f3aea71f626ebf202
FoxKing15 <emmie.bourassin@icloud.com>
2022-03-28 14:56:50 +0100

    update equidae class

--- Models/Equidea.model.cs
+++ Models/Equidea.model.cs
@@ -2,7 +2,11 @@
 {
     public class Equidea_model
     {
-        
+        public string Name { get; set; }    
+        public string Gender { get; set; }  
+        public int Age { get; set; }    
+        public string Color { get; set; }   
+        public string Type { get; set; }    
         public bool IsSick { get; set; }
         public int Hours { get; set; }
     }
