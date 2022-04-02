a2f406066efe8e3c4a162dc8886f3d80d70725f9
FoxKing15 <emmie.bourassin@icloud.com>
2022-04-02 12:27:18 +0100

    update equidea pop up & class

--- Models/Equidea.model.cs
+++ Models/Equidea.model.cs
@@ -2,7 +2,8 @@
 {
     public class Equidea_model
     {
-        public string Name { get; set; }    
+        public string Lastname { get; set; }   
+        public string Firstname { get; set; }   
         public string Gender { get; set; }  
         public int Age { get; set; }    
         public string Color { get; set; }   
