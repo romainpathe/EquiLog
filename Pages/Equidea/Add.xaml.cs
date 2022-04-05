6ade7a52b3d4d0f5c2654fcfeb8430aa1f7688c8
FoxKing15 <emmie.bourassin@icloud.com>
2022-04-05 23:51:07 +0100

    rectification button problem

--- Pages/Equidea/Add.xaml.cs
+++ Pages/Equidea/Add.xaml.cs
@@ -8,5 +8,10 @@ namespace EquiLog.Pages.Equidea
         {
             InitializeComponent();
         }
+
+        private void ButtonBase_OnClick(object sender, System.Windows.RoutedEventArgs e)
+        {
+
+        }
     }
 }
