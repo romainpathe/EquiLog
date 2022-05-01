9507caa3cc6b227cfe235dd2d47fa0f658e668f1
Romain Pathé <no_reply@romainpathe.com>
2022-05-01 15:48:31 +0200

    dd

---
+++ Pages/Planning.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace EquiLog.Pages
+{
+    public partial class Planning : Page
+    {
+        public Planning()
+        {
+            InitializeComponent();
+        }
+    }
+}
